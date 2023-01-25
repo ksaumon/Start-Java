68fa84d5005ce98bb7a343a5a415390800c301cf
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-01-25 22:33:10 +0300

    Initial houm vorc 1

---
+++ VariablesTheme.java
@@ -0,0 +1,22 @@
+public class VariablesTheme{
+	public static void main(String[] args) {
+        System.out.println("1 Вывод значений переменных на консоль");
+        byte numberofcores = 8;
+        System.out.println("numberofcores 8");
+        short cpufreguency = 3300;
+        System.out.println("cpufreguency 3300");
+        int harddisk = 512;
+        System.out.println("harddisk 512");
+        long sysnemtype = 64;
+        System.out.println("sysnemtype 64");
+        float ram = 16;
+        System.out.println("float 16");
+        double videocard = 5600;
+        System.out.println("videocard 5600");
+        char motherboard = 'L';
+        System.out.println("motherboard L");
+        boolean screen = true;
+        System.out.println("screen true");
+        
+    }
+}
