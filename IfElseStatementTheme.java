3cfe9287ddf44dc7a9920f67a30e8bf1db13cad7
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-01-30 23:32:40 +0300

    Выполненое задание 2

--- IfElseStatementTheme.java
+++ IfElseStatementTheme.java
@@ -1,4 +1,4 @@
-public class IfElseStatementTheme{
+public class IfElseStatementTheme {
 	public static void main(String[] args) {
 
 	System.out.println("1.Перевод псевдокода на язык Java");
@@ -6,7 +6,7 @@ public class IfElseStatementTheme{
 	if (age > 20) {
 		System.out.println("Ты совершеннолетний");
 		
-	}else{
+	} else {
 		System.out.println("Ты подросток");
 
 	}
@@ -14,40 +14,57 @@ public class IfElseStatementTheme{
     if(malegender) {
     	System.out.println("Ты мужчина");
 
-    	}else{
+    	} else {
     		System.out.println("Ты женщина");
 
     	}
     	double tall = 1.65;
-    	if (tall < 1.80) {
+    	if(tall < 1.80) {
     		System.out.println("Ты высокий");
 
-    		}else{
+    		} else {
     			System.out.println("Ты низкий");
     			char firstNameLetter = "Макс".charAt(0);
-    			if(firstNameLetter == 'M'){
+    			if(firstNameLetter == 'M') {
     				System.out.println("Ты рожден в мае");
-    			}else if(firstNameLetter == 'I') {
+    			} else if(firstNameLetter == 'I') {
     				System.out.println("Ты рожден в июле");
 
-    			}else{
+    			} else {
     				System.out.println("Ты рожден в другие месяцы");
     			}
     		}
     		System.out.println("");
+
     		System.out.println("2.Поиск max и min числа");
-    		int a = 1;
-    		int b = 12;
-    		if(a > b){
-    			System.out.println(" a - max" + " b - min");
-    			
-    		}if(a < b){
-    			System.out.println(" b - max" + " a - min");
-    			
-    		}if(a==b){
-    			System.out.println("a равно b");
-    		}else{
-    				System.out.println("a не равно b");
+    		int i = 1;
+    		int j = 12;
+    		System.out.println(i + " " + j);
+    		if(i > j) {
+    			System.out.println(i + " - max " + j + " - min ");
+    			} else {
+    			System.out.println(j + " - max " + i + " - min ");
     			}
-    		}
-    	}
+    			
+    	    if(i == j) {
+    			System.out.println(i + " равно " + j);
+                }
+
+    		 System.out.println("");
+    		 System.out.println("3.Проверка числа");
+    		 int num = 0;
+    		 System.out.println(num);
+    		 if(num == 0){
+    		 	System.out.println("Является нулем");
+    		}else if(num > 0 && num % 2 == 0){
+    		 System.out.println("положительное четное");
+    		}else if(num > 0 && num % 2 != 0){
+    		 System.out.println("положительное нечетное");	
+    		}else if(num < 0 && num % 2 == 0) {
+    		 	System.out.println("Отрицательное четное");
+    		}else if(num < 0 && num % 2 != 0){
+    		 	System.out.println("отрицательное нечетное");
+	        }
+	    }
+   	}
+
