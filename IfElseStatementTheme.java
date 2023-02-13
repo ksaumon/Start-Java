c0e0079abeecd889c7d4315d52c19fe434ffb3ac
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-02-13 11:48:37 +0300

    Исправление задание 2

--- IfElseStatementTheme.java
+++ IfElseStatementTheme.java
@@ -2,7 +2,7 @@ public class IfElseStatementTheme {
     public static void main(String[] args) {
         System.out.println("\n1.Перевод псевдокода на язык Java");
         int age = 21;
-        if (age > 20) {
+        if(age > 20) {
             System.out.println("Ты совершеннолетний");
         } else {
             System.out.println("Ты подросток");
