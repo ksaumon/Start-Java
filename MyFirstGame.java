aa998024cc321e96c6c4550298b766a3cd4f8ec8
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-03-05 19:38:38 +0300

    Исправление задание угадай число

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -1,30 +1,26 @@
 public class MyFirstGame {
     public static void main(String[] args) {
-        int unknownNumber = 34;
-        int userNumber = 1;
-        int max = 100;
-        int min = 1;
-        do {
-            if (userNumber > max || unknownNumber > max) {
-                System.out.println("Число не входит в полуинтервал ( 0 - 100]");
-                break;
-            } else if(userNumber < min || unknownNumber < min) {
-                System.out.println("Число не входит в полуинтервал ( 0 - 100]");
-                break;
-            } else if(userNumber < unknownNumber) {
-                userNumber = userNumber + 2;
-            } else if(userNumber > unknownNumber) {
-                userNumber--;
-            }else {
-                userNumber++;
-            }
-            if(userNumber > unknownNumber) {
-                System.out.println("число " + userNumber + " больше того, что загадал компьютер.");
-            } else if(userNumber < unknownNumber) {
-                System.out.println("число " + userNumber + " меньше того, что загадал компьютер.");
-            } else if (userNumber == unknownNumber) {
-                System.out.println("число " + userNumber + " загадал компьютер. \nВы победили!");
-            }
-        } while (userNumber != unknownNumber);
+        int unknownNumber = 100;
+        int playerNumber = 15;
+        int startRange = 100;
+        int endRange = 1;
+        if (playerNumber > startRange || playerNumber < endRange
+                    || unknownNumber > startRange || unknownNumber < endRange) {
+            System.out.println("Число не входит в полуинтервал ( 0 - 100]");
+        } else {
+            do {
+                if(playerNumber > unknownNumber) {
+                    System.out.println("число " + playerNumber + " больше того, что загадал компьютер.");
+                    playerNumber--;
+                } else {
+                    playerNumber++;
+                }
+                if(playerNumber < unknownNumber) {
+                    System.out.println("число " + playerNumber + " меньше того, что загадал компьютер.");
+                    playerNumber = playerNumber + 2;
+                }
+            } while (playerNumber != unknownNumber);
+            System.out.println("число " + playerNumber + " загадал компьютер. \nВы победили!");
+        }
     }
 }
