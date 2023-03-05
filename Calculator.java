51895f3eff19c7bb19320326d0576af16a7e8c01
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-03-05 19:41:57 +0300

    Исправление задание калькулятор

--- Calculator.java
+++ Calculator.java
@@ -2,35 +2,30 @@ public class Calculator {
     public static void main(String[] args) {
         int num = 50;
         int num1 = 5;
-        char div = '/';
-        char multi = '*';
-        char sum = '+';
-        char diff = '-';
-        char remainderDivision = '%';
-        char degree = '^';
-        int result = num + num1;
-        int result1 = num - num1;
-        int result2 = num * num1;
-        int result3 = num / num1;
-        long result4 = 1;
-        int result5 = num % num1;
-        for (int i = 1; i <= num1; i++) {
-            result4 *= num;
+        char mathematicalValue = ('/' | '*' | '+' | '-' | '%' | '^');
+        long value = '^';
+        if(value == '^') {
+            mathematicalValue = '^';
+            value = 1;
+            for(int i = 1; i <= num1; i++) {
+                value *= num;
+            }
+        } else if(value == '+') {
+            mathematicalValue = '+';
+            value = num + num1;
+        } else if(value == '-') {
+            mathematicalValue = '-';
+            value = num - num1;
+        } else if(value == '*') {
+            mathematicalValue = '*';
+            value = num * num1;
+        } else if(value == '/') {
+            mathematicalValue = '/';
+            value = num / num1;
+        } else if(value == '%') {
+            mathematicalValue = '%';
+            value = num % num1;
         }
-        long value = result4;
-        if (value == result) {
-            System.out.println(num + " " + sum + " " + num1 + " = " + result);
-        } else if(value == result1) {
-            System.out.println(num + " " + diff + " " + num1 + " = " + result1);
-        } else if(value == result2) {
-            System.out.println(num + " " + multi + " " + num1 + " = " + result2);
-        } else if(value == result3) {
-            System.out.println(num + " " + div + " " + num1 + " = " + result3);
-        } else if(value == result4){
-            System.out.println(num + " " + degree + " " + num1 + " = " + result4);
-        } else if(value == result5) {
-            System.out.println(num + " " + remainderDivision + " " + num1 + " = " + result5);
-        }
-
+        System.out.println(num + " " + mathematicalValue + " " + num1 + " = " + value);
     }
 }
