744d2575b9e953dc227edd513dccdfc4fec964a9
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-03-06 22:41:59 +0300

    Исправление задание угадай число

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -1,23 +1,21 @@
 public class MyFirstGame {
     public static void main(String[] args) {
-        int unknownNumber = 100;
-        int playerNumber = 15;
-        int startRange = 100;
-        int endRange = 1;
-        if (playerNumber > startRange || playerNumber < endRange
-                    || unknownNumber > startRange || unknownNumber < endRange) {
+        int unknownNumber = 20;
+        int playerNumber = 100;
+        int startRange = 1;
+        int endRange = 100;
+        if (playerNumber < startRange || playerNumber > endRange
+                    || unknownNumber < startRange || unknownNumber > endRange) {
             System.out.println("Число не входит в полуинтервал ( 0 - 100]");
         } else {
             do {
                 if(playerNumber > unknownNumber) {
                     System.out.println("число " + playerNumber + " больше того, что загадал компьютер.");
                     playerNumber--;
-                } else {
-                    playerNumber++;
                 }
                 if(playerNumber < unknownNumber) {
                     System.out.println("число " + playerNumber + " меньше того, что загадал компьютер.");
-                    playerNumber = playerNumber + 2;
+                    playerNumber++;
                 }
             } while(playerNumber != unknownNumber);
             System.out.println("число " + playerNumber + " загадал компьютер. \nВы победили!");
