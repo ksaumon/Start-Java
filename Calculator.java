80aaec1c370c0ec3ec79511b349d16963f2d7cbe
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-03-06 22:42:23 +0300

    Исправление задание калькулятор

--- Calculator.java
+++ Calculator.java
@@ -1,31 +1,32 @@
 public class Calculator {
     public static void main(String[] args) {
-        int num = 50;
+         int num = 50;
         int num1 = 5;
-        char mathematicalValue = ('/' | '*' | '+' | '-' | '%' | '^');
-        long value = '^';
-        if(value == '^') {
-            mathematicalValue = '^';
-            value = 1;
-            for(int i = 1; i <= num1; i++) {
-                value *= num;
+        char sign = '+';
+        int result = 0;
+        long mathematicalOperation = num + num1;
+        if(mathematicalOperation == num + num1) {
+            sign = '+';
+            result = num + num1;
+        } else if(mathematicalOperation == num - num1) {
+            sign = '-';
+            result = num - num1;
+        } else if(mathematicalOperation == num * num1) {
+            sign = '*';
+            result = num * num1;
+        } else if(mathematicalOperation == num / num1) {
+            sign = '/';
+            result = num / num1;
+        } else if(mathematicalOperation == num % num1) {
+            sign = '%';
+            result = num % num1;
+        } else if(mathematicalOperation == (num ^ num1 ^ num1)) {
+            sign = '^';
+            result = 1;
+            for (int i = 1; i <= num1; i++) {
+                result *= num;
             }
-        } else if(value == '+') {
-            mathematicalValue = '+';
-            value = num + num1;
-        } else if(value == '-') {
-            mathematicalValue = '-';
-            value = num - num1;
-        } else if(value == '*') {
-            mathematicalValue = '*';
-            value = num * num1;
-        } else if(value == '/') {
-            mathematicalValue = '/';
-            value = num / num1;
-        } else if(value == '%') {
-            mathematicalValue = '%';
-            value = num % num1;
         }
-        System.out.println(num + " " + mathematicalValue + " " + num1 + " = " + value);
+        System.out.println(num + " " + sign + " " + num1 + " = " + result);
     }
 }
