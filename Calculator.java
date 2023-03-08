55ad87a6c62dd56a9fea1731f343fab15f0a3f13
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-03-08 12:47:05 +0300

    Исправление задание калькулятор

--- Calculator.java
+++ Calculator.java
@@ -2,26 +2,19 @@ public class Calculator {
     public static void main(String[] args) {
          int num = 50;
         int num1 = 5;
-        char sign = '+';
+        char sign = '%';
         int result = 0;
-        long mathematicalExpression = num + num1;
-        if(mathematicalExpression == num + num1) {
-            sign = '+';
+        if(sign == '+') {
             result = num + num1;
-        } else if(mathematicalExpression == num - num1) {
-            sign = '-';
+        } else if(sign == '-') {
             result = num - num1;
-        } else if(mathematicalExpression == num * num1) {
-            sign = '*';
+        } else if(sign == '*') {
             result = num * num1;
-        } else if(mathematicalExpression == num / num1) {
-            sign = '/';
+        } else if(sign == '/') {
             result = num / num1;
-        } else if(mathematicalExpression == num % num1) {
-            sign = '%';
+        } else if(sign == '%') {
             result = num % num1;
-        } else if(mathematicalExpression == (num ^ num1 ^ num1)) {
-            sign = '^';
+        } else if(sign == '^') {
             result = 1;
             for (int i = 1; i <= num1; i++) {
                 result *= num;
