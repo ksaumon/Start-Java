5efce17928bcecf4ea55ccaed1483b0b554a6028
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-03-08 12:49:17 +0300

    Исправление задание калькулятор

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
     public static void main(String[] args) {
-         int num = 50;
+        int num = 50;
         int num1 = 5;
         char sign = '%';
         int result = 0;
