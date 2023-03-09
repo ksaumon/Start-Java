59d25265c24c24f47bf97d5a99e02eb015f0fdc7
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-03-09 19:19:59 +0300

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "man";
+    String name = "Richard";
+    int height = 177;
+    int weight = 85;
+    int age = 37;
+
+    void move() {
+        System.out.println("moving");
+    }
+
+    void sit() {
+        System.out.println("sit down");
+    }
+
+    void run() {
+        System.out.println("run away");
+    }
+
+    void speak() {
+        System.out.println("to talk");
+    }
+
+    void study() {
+        System.out.println("study Java");
+    }
+}
