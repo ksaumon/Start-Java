f12874611739335fbed1cb5c6f8c7267c8aae809
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-03-12 17:47:35 +0300

    Выполнение задание 21

---
+++ less1/CyclesTheme.java
@@ -0,0 +1,185 @@
+public class CyclesTheme {
+    public static void main(String[] args) {
+        System.out.println("\n1.Подсчет суммы четных и нечетных чисел");
+        int sumEvenNums = 0;
+        int sumOddNums = 0;
+        int rangeBegin = -10;
+        int rangeEnd = 21;
+        do {
+            if(rangeBegin % 2 == 0) {
+                sumEvenNums += rangeBegin;
+            } else {
+                sumOddNums += rangeBegin;
+            }
+            rangeBegin++;
+        } while(rangeBegin <= rangeEnd);
+        System.out.println("в промежутке [-10, 21] сумма четных чисел = "
+                + sumEvenNums + " нечетных = " + sumOddNums);
+
+        System.out.println("\n2.Вывод чисел в интервале (min и max) в порядке убывания");
+        int num = -1;
+        int num1 = 5;
+        int num2 = 10;
+        int max = num2;
+        int min = num1;
+        if(num1 > max) {
+            max = num1;
+        }
+        if(num > max) {
+            max = num;
+        }
+        if(num2 < min) {
+            min = num2;
+        }
+        if(num < min) {
+            min = num;
+        }
+        for(int i = --max; i > min; i--) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\n" + "\n3.Вывод реверсивного числа и суммы его цифр");
+        int num3 = 1234;
+        int sum = 0;
+        while(num3 > 0) {
+            int digit = num3 % 10;
+            sum += digit;
+            num3 /= 10;
+            System.out.print(digit + "");
+        }
+        System.out.println(" исходное число в обратном порядке");
+        System.out.println(sum + " сумма его цифр");
+
+        System.out.println("\n4.Вывод чисел на консоль в несколько строк");
+        int num4 = 24;
+        int count = 0;
+        for(int i = 1; i < num4; i += 2) {
+            System.out.printf("%4d", i);
+            count++;
+            while(count == 5) {
+                System.out.println();
+                count = 0;
+            }
+        }
+        for(int i = count; i < 5; i++) {
+            if(count == 0) break;
+            System.out.printf("%4d", 0);
+        }
+
+        System.out.println("\n" + "\n5.Проверка количества двоек на четность/нечетность");
+        int num5 = 1252292;
+        int copyNum5 = num5;
+        int countTwos = 0;
+        while(num5 > 0) {
+            if((num5 % 10) == 2) {
+                countTwos++;
+            }
+            num5 /= 10;
+        }
+        if(countTwos % 2 == 0) {
+            System.out.println("число  " + copyNum5 + " содержит :" + countTwos + " (четное) - количество двоек");
+        } else {
+            System.out.println("число  " + copyNum5 + " содержит :" + countTwos + " (нечетное) - количество двоек");
+        }
+
+        System.out.println("\n6.Отображение фигур в консоли\n");
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        int countLines = 5;
+        int countColumns = 0;
+        do {
+            System.out.print("#");
+            countColumns++;
+            if(countColumns == countLines) {
+                countLines--;
+                System.out.println();
+                countColumns = 0;
+            }
+        } while(countColumns < countLines);
+        System.out.println();
+
+        countLines = 1;
+        do {
+            countColumns = 1;
+            do {
+                System.out.print("$");
+                countColumns++;
+            } while(countColumns <= countLines && countColumns <= 6 - countLines);
+            System.out.println();
+            countLines++;
+        } while(countLines <= 5);
+
+        System.out.println("\n7.Отображение ASCII-символов");
+         System.out.println("Dec" + "\tChar");
+        for(int i = 1; i <= 47; i += 2) {
+            System.out.printf("%3d %8s\n", i, (char) i);
+        }
+        System.out.println("\n" + "Dec" + "\tChar");
+        for(int i = 98; i <= 122; i += 2) {
+            System.out.printf("%3d %8s\n", i, (char) i);
+        }
+
+        System.out.println("\n8.Проверка, является ли число палиндромом");
+        int num6 = 1234321;
+        int reverseNum6 = 0;
+        int copyNum6 = num6;
+        while(num6 != 0) {
+            int digit = num6 % 10;
+            reverseNum6 = reverseNum6 * 10 + digit;
+            num6 /= 10;
+        }
+        if(reverseNum6 == copyNum6) {
+            System.out.println("Число " + copyNum6 + " является палиндромом");
+        } else {
+            System.out.println("Число " + copyNum6 + " не является палиндромом");
+        }
+
+        System.out.println("\n9.Определение, является ли число счастливым");
+        int num7 = 423321;
+        int leftHalfNum7 = num7 / 1000;
+        int rightHalfNum7 = num7 % 1000;
+        int sumLeftHalf = 0;
+        int sumRightHalf = 0;
+        int copyRightHalfNum7 = rightHalfNum7;
+        for(int i = leftHalfNum7; i > 0; i /=10) {
+            int digit = i % 10;
+            sumLeftHalf += digit;
+            int digit1 = copyRightHalfNum7 % 10;
+            sumRightHalf += digit1;
+            if(copyRightHalfNum7 == rightHalfNum7 ) {
+                copyRightHalfNum7 = rightHalfNum7 / 10;
+            } else if(copyRightHalfNum7 == rightHalfNum7 / 10) {
+                copyRightHalfNum7 = rightHalfNum7 / 100;
+            }
+        }
+        if(sumLeftHalf != sumRightHalf) {
+            System.out.println(num7 + " число не является счастливым");
+        } else {
+            System.out.print("Сумма цифр " + leftHalfNum7 + " = "
+                    + sumLeftHalf + "; Сумма цифр " + rightHalfNum7 + " = " + sumRightHalf + "\n");
+            System.out.println(num7 + " число является счастливым");
+        }
+
+        System.out.println("\n10.Вывод таблицы умножения Пифагора\n");
+        System.out.println("    ТАБЛИЦА" + "    ПИФАГОРА\n");
+        System.out.print("   |");
+        for(int i = 2; i < 10; i++) {
+            System.out.printf("%2d ", i);
+        }
+        System.out.printf("\n___|___");
+        System.out.printf("____________________\n");
+        for(int i = 2; i < 10; i++) {
+            System.out.printf("%2d |", i);
+            for(int j = 2; j < 10; j++) {
+                System.out.printf("%2d ", i * j);
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ less1/VariablesTheme.java
@@ -0,0 +1,123 @@
+public class VariablesTheme {
+    public static void main(String[] args) {
+        System.out.println("\n1.Вывод значений переменных на консоль");
+        byte numberOfcores = 8;
+        System.out.println("numberOfcores 8");
+        short cpuFreguency = 3300;
+        System.out.println("cpuFreguency 3300");
+        int hardDisk = 512;
+        System.out.println("harDisk 512");
+        long systemType = 64;
+        System.out.println("sysnemType 64");
+        float ram = 16;
+        System.out.println("float 16");
+        double videoCard = 5600;
+        System.out.println("videoCard 5600");
+        char motherBoard = 'L';
+        System.out.println("motherboard L");
+        boolean screen = true;
+        System.out.println("screen true");
+
+        System.out.println("\n2.Расчет стоимости товара со скидкой");
+        int pricePen = 100;
+        int priceBook = 200;
+        int sumGoods = pricePen + priceBook;
+        int discount = sumGoods * 11 / 100;
+        System.out.println("Общая стоимость товаров без скидки = " + sumGoods);
+        System.out.println("Сумма скидки = " + discount);
+        System.out.println("Общая стоимость товаров со скидкой = " + (sumGoods - discount));
+
+        System.out.println("\n3 Вывод слова JAVA");
+        System.out.println("   J   a v     V a");
+        System.out.println("   J  a a v   v a a");
+        System.out.println("J  J aaaaa V V aaaaa");
+        System.out.println(" JJ a     a V a     a");
+
+        System.out.println("\n4.Вывод min и max значений целых числовых типов");
+        byte maxValueByte = 127;
+        short maxValueShort = 32767;
+        int maxValueInt = 2147483647;
+        long maxValuelong = 9223372036854775807L;
+        System.out.println(maxValueByte);
+        System.out.println(++maxValueByte + " " + --maxValueByte);
+        System.out.println(maxValueShort);
+        System.out.println(++maxValueShort + " " + --maxValueShort);
+        System.out.println(maxValueInt);
+        System.out.println(++maxValueInt + " " + --maxValueInt);
+        System.out.println(maxValuelong);
+        System.out.println(++maxValuelong + " " + --maxValuelong);
+
+        System.out.println("\n5.Перестановка значений переменных");
+        int number1 = 2;
+        int number2 = 5;
+        int temp = number1;
+        System.out.println("с помощью третьей переменной");
+        System.out.println("number1 =" + number1);
+        System.out.println("number2 =" + number2);
+        number1 = number2;
+        number2 = temp;
+        System.out.println("number1 =" + number1);
+        System.out.println("number2 =" + number2);
+        System.out.println("с помощью арифметических операций");
+        System.out.println("number1 =" + number1);
+        System.out.println("number2 =" + number2);
+        number1 -= number2;
+        number2 += number1;
+        number1 = number2 - number1;
+        System.out.println("number1 =" + number1);
+        System.out.println("number2 =" + number2);
+        System.out.println("с помощью побитовой операции ^");
+        System.out.println("number1 =" + number1);
+        System.out.println("number2 =" + number2);
+        number1 ^= number2;
+        number2 ^= number1;
+        number1 ^= number2;
+        System.out.println("number1 =" + number1);
+        System.out.println("number2 =" + number2);
+
+        System.out.println("\n6.Вывод символов и их кодов");
+        char lattice = '#';
+        char end = '&';
+        char at = '@';
+        char caret = '^';
+        char underscore = '_';
+        System.out.println((int) lattice + " " + lattice);
+        System.out.println((int) end + " " + end);
+        System.out.println((int) at + " " + at);
+        System.out.println((int) caret + " " + caret);
+        System.out.println((int) underscore + " " + underscore);
+
+        System.out.println("\n7.Вывод в консоль ASCII-арт Дюка");
+        char slash = '/';
+        char backslash = '\\';
+        char leftParentheses = '(';
+        char rightParentheses = ')';
+        char underscore1 = '_';
+        System.out.println("        " + slash + backslash);
+        System.out.println("       " + slash + "  " + backslash);
+        System.out.println("      " + slash + underscore1 + leftParentheses + " " + rightParentheses
+                + backslash);
+        System.out.println("     " + slash + "      " + backslash);
+        System.out.println("    " + slash + underscore1 + underscore1 + underscore1 + underscore1
+                + slash + backslash + underscore1 + underscore1+ backslash);
+
+        System.out.println("\n8.Вывод количества сотен, десятков и единиц числа");
+        int number = 123;
+        int hundreds = number / 100;
+        int tens = number % 100 / 10;
+        int ones = number % 10;
+        System.out.println("Число " + number + " содержит");
+        System.out.println(hundreds+ " Сотню");
+        System.out.println(tens + " Десятки"); 
+        System.out.println(ones + " Единицы");
+        System.out.println("Сумму его цифр = " + (hundreds + tens + ones));
+        System.out.println("Произведение = " + hundreds * tens * ones);
+
+        System.out.println("\n9.Вывод времени");
+        int srcSeconds = 86399;
+        int hour = srcSeconds / 3600;
+        int minutes = srcSeconds / 60 % 60;
+        int seconds = srcSeconds % 60;
+        System.out.println(hour + ":" + minutes + ":" + seconds);
+    }
+}
