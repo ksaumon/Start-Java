e7a8c3c86e42c9746fa06ce02c498873585bd7c6
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-03-12 17:49:46 +0300

    Выполнение задание 21

---
+++ less2/Wolf.java
@@ -0,0 +1,27 @@
+public class Wolf {
+    String gender = "man";
+    String nickName = "Richard";
+    int weight = 14;
+    int age = 5;
+    String color =  "grey";
+
+    void walk() {
+        System.out.println("walk");
+    }
+
+    void sit() {
+        System.out.println("sit down");
+    }
+
+    void run() {
+        System.out.println("run");
+    }
+
+    void howl() {
+        System.out.println("howl");
+    }
+
+    void hunt() {
+        System.out.println("hunt");
+    }
+}
