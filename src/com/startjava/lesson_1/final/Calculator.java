24a09fe61b7d1dcfa260bf0a5de5e70250deefff
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-04-28 14:55:49 +0300

    Выполнение итогового задание

---
+++ src/com/startjava/lesson_1/final/Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int num = 50;
+        int num1 = 5;
+        char sign = '%';
+        int result = 0;
+        if(sign == '+') {
+            result = num + num1;
+        } else if(sign == '-') {
+            result = num - num1;
+        } else if(sign == '*') {
+            result = num * num1;
+        } else if(sign == '/') {
+            result = num / num1;
+        } else if(sign == '%') {
+            result = num % num1;
+        } else if(sign == '^') {
+            result = 1;
+            for (int i = 1; i <= num1; i++) {
+                result *= num;
+            }
+        }
+        System.out.println(num + " " + sign + " " + num1 + " = " + result);
+    }
+}
