8db14a9d98aad034ef122d7ee46243015ea7a367
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-05-14 13:07:51 +0300

    Выполнение урок 3

--- src/com/startjava/lesson_1/final/Calculator.java
+++ src/com/startjava/lesson_1/final/Calculator.java
@@ -1,4 +1,4 @@
-package com.startjava.lesson_1.Final;
+package com.startjava.lesson_1.final_;
 
 public class Calculator {
     public static void main(String[] args) {
--- src/com/startjava/lesson_1/final/MyFirstGame.java
+++ src/com/startjava/lesson_1/final/MyFirstGame.java
@@ -1,4 +1,4 @@
-package com.startjava.lesson_1.Final;
+package com.startjava.lesson_1.final_;
 
 public class MyFirstGame {
     public static void main(String[] args) {
