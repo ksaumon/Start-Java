bec4749e22f8c2bfdb79e4c287cc0729e089da88
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-05-15 21:22:55 +0300

    Выполнение урок 4

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -2,6 +2,6 @@ package com.startjava.lesson_2_3_4.array;
 
 public class ArrayTheme {
     public static void main(String[] args){
-        System.out.println("12");
+        System.out.println("1234");
     }
 }
