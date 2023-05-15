3b8cb17dbfab34b4d5fbd7d0a46b97570693ff49
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-05-15 22:56:35 +0300

    Выполнение урок 4

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -1,7 +1,26 @@
 package com.startjava.lesson_2_3_4.array;
 
 public class ArrayTheme {
-    public static void main(String[] args){
-        System.out.println("1234");
+    public static void main(String[] args) {
+       rev();
+       mult();
     }
+
+        public static void rev() {
+            System.out.println("1.Реверс значений массива");
+            int[] num = {2, 1, 3, 5, 4, 7, 6};
+            for (int num1 : num) {
+                System.out.print(num1 + " ");
+            }
+            System.out.println();
+            for (int i = num.length - 1; i >= 0; i--) {
+                System.out.print(num[i] + " ");
+            }
+            System.out.println("\n");
+        }
+
+        public static void mult() {
+            System.out.println("2.Вывод произведения элементов массива");
+        }
+
 }
