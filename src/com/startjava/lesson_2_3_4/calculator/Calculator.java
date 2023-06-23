a30ca75e6225f114935864c75db613cc9e733124
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-06-23 18:50:32 +0300

    Выполнение урок 4.

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,32 +1,29 @@
 package com.startjava.lesson_2_3_4.calculator;
 
-import static com.startjava.lesson_2_3_4.calculator.CalculatorTest.str;
-
 public class Calculator {
-    private static double num;
-    private static double num1;
+    private static int num;
+    private static int num1;
     private static char sign;
-    static double result;
 
     public static double calculate() {
-        String str1 = str;
+        String str1 = CalculatorTest.str;
         String[] elements = str1.split(" ");
-        Calculator.num = Integer.parseInt(elements[0]);
-        Calculator.sign = elements[1].charAt(0);
-        Calculator.num1 = Integer.parseInt(elements[2]);
+        num = Integer.parseInt(elements[0]);
+        sign = elements[1].charAt(0);
+        num1 = Integer.parseInt(elements[2]);
         switch (sign) {
             case '+':
-                return Calculator.result = Math.addExact((int) num,(int) num1);
+                return Math.addExact(num, num1);
             case '-':
-                return Calculator.result = Math.subtractExact((int) num, (int) num1);
+                return Math.subtractExact(num, num1);
             case '*':
-                return Calculator.result = Math.multiplyExact((int) num, (int) num1);
+                return Math.multiplyExact(num, num1);
             case '/':
-                return Calculator.result = num / num1;
+                return (double) num / (double) num1;
             case '%':
-                return Calculator.result = num % num1;
+                return num % num1;
             case '^':
-                return Calculator.result = Math.pow(num, num1);
+                return Math.pow(num, num1);
         }
         return 0;
     }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -2,27 +2,29 @@ package com.startjava.lesson_2_3_4.calculator;
 
 import java.util.Scanner;
 
-import static com.startjava.lesson_2_3_4.calculator.Calculator.calculate;
-import static com.startjava.lesson_2_3_4.calculator.Calculator.result;
-
 public class CalculatorTest {
     static String str;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String actionChoice = "yes";
+        String option = "yes";
         do {
-            if("yes".equals(actionChoice)) {
+            if("yes".equals(option)) {
                 System.out.print("Введите математическое выражение: ");
                 str = scanner.nextLine();
-                calculate();
-                if(result != result % 2) {
-                    System.out.println((int) result);
-                } else {
-                    System.out.printf("%1.3f \n", result);
-                }
+                print();
             }
             System.out.print("Хотите продолжить вычисления? [yes/no]:");
-            actionChoice = scanner.nextLine();
-        } while(!"no".equals(actionChoice));
+            option = scanner.nextLine();
+        } while(!"no".equals(option));
+    }
+
+    static double print() {
+        double result = Calculator.calculate();
+        if(result % 2 == 0) {
+            System.out.println((int) result);
+        } else {
+            System.out.printf("%1.3f \n", result);
+        }
+        return result;
     }
 }
