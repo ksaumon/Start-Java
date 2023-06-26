441821531f960ffd1dc224f251a82dec31338fdf
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-06-26 21:28:11 +0300

    Выполнение урок 4.

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,30 +1,43 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+
 public class Calculator {
     private static int num;
     private static int num1;
     private static char sign;
 
-    public static double calculate() {
-        String str1 = CalculatorTest.str;
+    public static double calculate(String str) {
+        String str1 = str;
+        try {
+            checkingNumber(str1);
+        } catch(RuntimeException e) {
+            return -0.1;
+        }
+        double result = switch(sign) {
+            case '+' -> Math.addExact(num, num1);
+            case '-' -> Math.subtractExact(num, num1);
+            case '*' -> Math.multiplyExact(num, num1);
+            case '/' -> (double) num / (double) num1;
+            case '%' -> num % num1;
+            case '^' -> Math.pow(num, num1);
+            default -> {yield -0.1;}
+        };
+        return result;
+    }
+
+    public static double checkingNumber( String str1) {
         String[] elements = str1.split(" ");
+        double num2 = Double.parseDouble(elements[0]);
+        double num3 = Double.parseDouble(elements[2]);
+        if(num2 % 1 != 0 || num3 % 1 != 0) {
+            throw new RuntimeException();
+        }
         num = Integer.parseInt(elements[0]);
         sign = elements[1].charAt(0);
         num1 = Integer.parseInt(elements[2]);
-        switch (sign) {
-            case '+':
-                return Math.addExact(num, num1);
-            case '-':
-                return Math.subtractExact(num, num1);
-            case '*':
-                return Math.multiplyExact(num, num1);
-            case '/':
-                return (double) num / (double) num1;
-            case '%':
-                return num % num1;
-            case '^':
-                return Math.pow(num, num1);
+        if(num < 0 || num1 < 0) {
+            throw new RuntimeException();
         }
-        return 0;
+        return num + sign + num1;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -3,22 +3,21 @@ package com.startjava.lesson_2_3_4.calculator;
 import java.util.Scanner;
 
 public class CalculatorTest {
-    static String str;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String option = "yes";
         do {
-            if ("yes".equals(option)) {
+            if("yes".equals(option)) {
                 System.out.print("Введите математическое выражение: ");
-                str = scanner.nextLine();
-                double result = Calculator.calculate();
-                if (result % 1 == 0) {
-                    System.out.println((int) result);
+                String str = scanner.nextLine();
+                double result = Calculator.calculate(str);
+                if(result == -0.1) {
+                    System.out.println("Введины некоректные данные");
                 } else {
-                    System.out.printf("%1.3f \n", result);
+                    String choiceRecordingForm = ((result % 1) == 0) ? String.format("%1.0f", result)
+                            : String.format("%1.3f", result);
+                    System.out.println(choiceRecordingForm);
                 }
-//                String choiceRecordingForm = ((result % 1) == 0) ? String.format("%1.0f", result) : String.format("%1.3f", result);
-//                System.out.println(choiceRecordingForm);
             }
             System.out.print("Хотите продолжить вычисления? [yes/no]:");
             option = scanner.nextLine();
