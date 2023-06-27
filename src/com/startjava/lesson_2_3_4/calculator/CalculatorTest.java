972289d0a334a153029a4299fbeeee5370a3415f
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-06-27 18:36:47 +0300

    Выполнение урок 4.

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -6,21 +6,24 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String option = "yes";
+        double result = 0;
         do {
-            if("yes".equals(option)) {
-                System.out.print("Введите математическое выражение: ");
-                String str = scanner.nextLine();
-                double result = Calculator.calculate(str);
-                if(result == -0.1) {
-                    System.out.println("Введины некоректные данные");
-                } else {
-                    String choiceRecordingForm = ((result % 1) == 0) ? String.format("%1.0f", result)
-                            : String.format("%1.3f", result);
-                    System.out.println(choiceRecordingForm);
+            do {
+                if("yes".equals(option)) {
+                    System.out.print("Введите математическое выражение: ");
+                    String str = scanner.nextLine();
+                    result = Calculator.calculate(str);
+                    if(result == -0.1) {
+                        System.out.println("Введины некоректные данные");
+                    } else {
+                        String choiceRecordingForm = result % 1 == 0 ? String.format("%1.0f", result)
+                                : String.format("%1.3f", result);
+                        System.out.println(choiceRecordingForm);
+                    }
                 }
-            }
+            } while(result == -0.1);
             System.out.print("Хотите продолжить вычисления? [yes/no]:");
             option = scanner.nextLine();
-        } while (!"no".equals(option));
+        } while(!"no".equals(option));
     }
 }
