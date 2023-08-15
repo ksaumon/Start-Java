2009d4fe2a49fa58fdcd4078db51b74f4ef8ce4e
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-08-15 20:29:38 +0300

    Выполнение выпускного проекта

--- src/com/startjava/graduation/bookshelf/Bookshelf.java
+++ src/com/startjava/graduation/bookshelf/Bookshelf.java
@@ -1,6 +1,5 @@
 package com.startjava.graduation.bookshelf;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Bookshelf {
@@ -9,31 +8,68 @@ public class Bookshelf {
     Scanner scanner = new Scanner(System.in);
     Book book = new Book();
     private int numberBooks;
+    private int numberShelves;
+
+    public int getNumberBooks() {
+        return numberBooks;
+    }
+
+    public int freeNumberShelves() {
+        return CAPACITY - numberShelves;
+    }
+
+    public void book() {
+
+    }
 
     public void saveBooks() {
-        System.out.println("Введите автора, ");
+        System.out.println("Введите автора");
         book.setAuthor(scanner.nextLine());
-        System.out.println("Введите название книги, ");
+        System.out.println("Введите название книги");
         book.setName(scanner.nextLine());
         System.out.println("Введите год издания");
         book.setYearsPublishing(scanner.nextInt());
-        books[numberBooks] = book.getAuthor() + book.getName() + book.getYearsPublishing();
-        deleteBooks();
+        books[numberBooks] = book.line();
+        numberBooks++;
+        printBookshelf();
     }
 
     public void findBooks() {
-
+        System.out.println("Введите название книги");
+        scanner.nextLine();
+        String findNameBook = scanner.nextLine();
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] == findNameBook) {
+                System.out.println("Данная книга найдена" + books[numberBooks]);
+                break;
+            }
+        }
+        System.out.println("Данная книга не найдена");
     }
 
     public void deleteBooks() {
+        System.out.println("Введите название книги");
+        scanner.nextLine();
+        String findNameBook = scanner.nextLine();
+        for (int i = 0; i < books.length; i++) {
+            if (books[i].bgetName == findNameBook) {
+                books[i] = null;
+            } else {
+                System.out.println("Книга не найдена");
+            }
+        }
+
+    }
+
+    public void printBookshelf() {
         for (int i = 0; i < books.length; i++) {
             if (books[i] != null){
-                System.out.println(books[i]);
+                System.out.println(books[i] + "");
             }
         }
     }
 
-    public void getAllBooks() {
+    public void clearBookshelf() {
 
     }
 
--- src/com/startjava/graduation/bookshelf/BookshelfTest.java
+++ src/com/startjava/graduation/bookshelf/BookshelfTest.java
@@ -4,12 +4,21 @@ import java.util.Scanner;
 
 public class BookshelfTest {
 
-    public static void main(String[] args) {
+    private static final int quit = 5;
+
+    public static void main( String[] args) {
         Scanner scanner = new Scanner(System.in);
         Bookshelf bookshelf = new Bookshelf();
-        System.out.println("Шкаф пуст. Вы можете добавить в него первую книгу");
-        menu();
-        bookshelf.saveBooks();
+        if (bookshelf.getNumberBooks() == 0) {
+            System.out.println("Шкаф пуст. Вы можете добавить в него первую книгу");
+        } else {
+            System.out.println("В шкафу книг -" + bookshelf.getNumberBooks() +
+                    ", Свободных полок -" + bookshelf.freeNumberShelves());
+        }
+        do {
+            menu();
+           // chooseMenuItem(scanner, bookshelf);
+        }while(chooseMenuItem(scanner, bookshelf) != quit);
     }
 
     public static void menu() {
@@ -21,5 +30,25 @@ public class BookshelfTest {
           5. quit
           """);
         System.out.println("Введите номер команды");
+    }
+
+    public static int chooseMenuItem(Scanner scanner, Bookshelf bookshelf) {
+        int itemMenu = scanner.nextInt();
+        if(itemMenu == 1) {
+            bookshelf.saveBooks();
+        }
+        if(itemMenu == 2) {
+            bookshelf.deleteBooks();
+        }
+        if(itemMenu == 3) {
+            bookshelf.findBooks();
+        }
+        if(itemMenu == 4) {
+            bookshelf.clearBookshelf();
+        }
+        if(itemMenu == 5) {
+            return quit;
+        }
+        return itemMenu;
     }
 }
